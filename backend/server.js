f471c8a7d5e2fc155fadfca36a7ebd6a8b4d530e
Chanuka999 <chanukaranditha99@gmail.com>
2025-11-12 14:29:51 +0530

    feat: enhance CORS middleware to allow specific origins and enable credentials

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,33 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDb } from "./config/Db.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigin = ["http://localhost:5173", "http://localhost:5174"];
+      if (!origin || allowedOrigin.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by cors"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDb();
 
 //routes
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("Api worked");
 });
