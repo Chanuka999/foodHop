1ad1c87bb7fb5332df759952a76c4ebd1a573ce9
Chanuka999 <chanukaranditha99@gmail.com>
2025-11-17 10:38:26 +0530

    fix: correct filename property in multer storage configuration

--- backend/routes/itemRoute.js
+++ backend/routes/itemRoute.js
@@ -10,8 +10,7 @@ const itemRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) =>
-    cb(null, `${Date.now()}-${file.fileoriginalname}`),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 
 const upload = multer({ storage });
